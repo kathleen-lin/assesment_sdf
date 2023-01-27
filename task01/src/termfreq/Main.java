c0e7dfbfe83a7229ba41087040079dbe9b9ae169
shuning <linshuning21@gmail.com>
2023-01-27 15:20:26 +0800

    task01 completed

--- task01/src/termfreq/Main.java
+++ task01/src/termfreq/Main.java
@@ -1,20 +1,20 @@
 package termfreq;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException{
         
         // read the text into memory while storing them into a map
-        System.out.println("Hello world");
         
         Map<String, Integer> wordCount = new HashMap<>();
         int totalWord = 0;
@@ -51,21 +51,27 @@ public class Main {
 
                 }
         }
-
-        // sort the map and print out
-//         Map<String, Integer> sortedFrequncy = budget .entrySet() .stream() .sorted(comparingByValue()) .collect( toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2, LinkedHashMap::new));
-
-// Read more: https://javarevisited.blogspot.com/2017/09/java-8-sorting-hashmap-by-values-in.html#ixzz7rZ4ZvQK7
-        
-
-
         System.out.println(totalWord);
-        System.out.println(Arrays.asList(wordCount)); // method 1
-    
+        System.out.println(Arrays.asList(wordCount));
+
+        // sort the map and print out top 10 (modify the number to frquency)
+        LinkedHashMap<String, Integer> reverseSortedMap = new LinkedHashMap<>();
+ 
+        wordCount.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).forEachOrdered(word -> reverseSortedMap.put(word.getKey(), word.getValue()));
+
+        System.out.println(Arrays.asList(reverseSortedMap));
+
+        // got a LinkedHashMap name reverseSortedMap
+        for (int i = 1; i < 11; i++){
+            Map.Entry<String,Integer> topWords = reverseSortedMap.entrySet().iterator().next();
+            String key= topWords.getKey();
+            Integer value=topWords.getValue();
+            float frequency = (float) value/totalWord;
+            System.out.printf("%d. Word: \"%s\" Frequency: %f\n", i, key, frequency);
+            reverseSortedMap.remove(key);
         }
-
-
+        br.close();
     }
-
+}
 
 
